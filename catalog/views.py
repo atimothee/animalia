cac25f678e94fc75a9d476cd11d61339ad022c07
Timothy Asiimwe <atimothee@gmail.com>
2014-10-08 15:02:34 +0300

    added animal filter form

--- catalog/views.py
+++ catalog/views.py
@@ -5,5 +5,5 @@ def home(request):
     return render_to_response('home.html', {})
 
 def animal_list(request):
-    animals = Animal.objects.filter(name__icontains=request.GET.get('name', ''))
+    animals = Animal.objects.filter(name__icontains=request.GET.get('name', ''), species__genus__family__name__icontains=request.GET.get('family', ''))
     return render_to_response('catalog/catalog_list.html', {'animals': animals})
